c6db0980f0278018e0c9914f715b889ba5023485
t0theheart <35728736+t0theheart@users.noreply.github.com>
2021-11-06 15:47:22 +0300

    some refactoring and teach snake to move!

--- snakes_game_service/app/static/js/canvas.js
+++ snakes_game_service/app/static/js/canvas.js
@@ -4,25 +4,24 @@ const canvas = document.getElementById("myCanvas");
 let x = canvas.width/5;
 let y = canvas.height-90;
 
-// const head = new SnakeSquareHead(x, y, 30, "#FF0000", 1, "#000000")
 const snake = new Snake(x, y, 30, 2, "#FF0000", 1, "#000000")
 
-// document.addEventListener("keyup", keyUpHandler, false);
-//
-// function keyUpHandler(e) {
-//     console.log(e)
-//     if (e.key === "ArrowUp") {
-//         head.moveUp()
-//     }
-//     if (e.key === "ArrowDown") {
-//         head.moveDown()
-//     }
-//     if (e.key === "ArrowRight") {
-//         head.moveRight()
-//     }
-//     if (e.key === "ArrowLeft") {
-//         head.moveLeft()
-//     }
-// }
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyUpHandler(e) {
+    console.log(e)
+    if (e.key === "ArrowUp") {
+        snake.moveUp()
+    }
+    if (e.key === "ArrowDown") {
+        snake.moveDown()
+    }
+    if (e.key === "ArrowRight") {
+        snake.moveRight()
+    }
+    if (e.key === "ArrowLeft") {
+        snake.moveLeft()
+    }
+}
 
 snake.create()
--- snakes_game_service/app/static/js/snake.js
+++ snakes_game_service/app/static/js/snake.js
@@ -9,57 +9,73 @@ export class Snake {
         this.borderWidth = borderWidth
         this.borderColor = borderColor
         this.body = []
-        this.head = null
-        this.headX = x
-        this.headY = y
-        this.createHead()
-        this.createStartBody()
+        this.createStartBody(x, y)
+
+        this.moveMap = {
+            up: {x: 0, y: -this.bodySize},
+            down: {x: 0, y: this.bodySize},
+            right: {x: this.bodySize, y: 0},
+            left: {x: -this.bodySize, y: 0}
+        }
     }
 
-    createHead () {
-        let head = new SquareObject(this.headX, this.headY, this.bodySize, this.bodyColor, this.borderWidth, this.borderColor)
+    createStartBody(x, y) {
+        let head = new SquareObject(x, y, this.bodySize, this.bodyColor, this.borderWidth, this.borderColor)
         this.body.push(head)
-    }
-
-    createStartBody () {
         for (let i = this.bodyLength; i > 0; i--) {
-            let lastElementY = this.body[this.body.length-1].y + this.bodySize
-            let lastElementX = this.body[this.body.length-1].x
-            let bodyElement = new SquareObject(lastElementX, lastElementY, this.bodySize, this.bodyColor, this.borderWidth, this.borderColor)
+            let lastBodyElementY = this.getLastBodyElement().y + this.bodySize
+            let lastBodyElementX = this.getLastBodyElement().x
+            let bodyElement = new SquareObject(lastBodyElementX, lastBodyElementY, this.bodySize, this.bodyColor, this.borderWidth, this.borderColor)
             this.body.push(bodyElement)
         }
     }
 
     create() {
-        this.body.forEach(bodyElement => {
-            bodyElement.draw()
-        })
+        this.body.forEach(bodyElement => {bodyElement.draw()})
+    }
+
+    getLastBodyElement() {
+        return this.body[this.body.length-1]
+    }
+
+    getFirstBodyElement() {
+        return this.body[0]
+    }
+
+    moveLastBodyElementToFirstBodyElement() {
+        let lastBodyElement = this.body.pop()
+        this.body.unshift(lastBodyElement)
+    }
+
+    moveUp () {
+        let move = this.moveMap["up"]
+        this.move(move.x, move.y)
+    }
+
+    moveDown() {
+        let move = this.moveMap["down"]
+        this.move(move.x, move.y)
+    }
+
+    moveRight() {
+        let move = this.moveMap["right"]
+        this.move(move.x, move.y)
+    }
+
+    moveLeft() {
+        let move = this.moveMap["left"]
+        this.move(move.x, move.y)
+    }
+
+    move(x, y) {
+        let lastBodyElement = this.getLastBodyElement()
+        let firstBodyElement = this.getFirstBodyElement()
+        lastBodyElement.clear()
+        lastBodyElement.x = firstBodyElement.x
+        lastBodyElement.y = firstBodyElement.y
+        lastBodyElement.x = lastBodyElement.x + x
+        lastBodyElement.y = lastBodyElement.y + y
+        lastBodyElement.draw()
+        this.moveLastBodyElementToFirstBodyElement()
     }
 }
-
-
-// export class SnakeSquareHead extends SquareObject {
-//     moveUp () {
-//         this.clear()
-//         this.y = this.y - this.sideLength
-//         this.draw()
-//     }
-//
-//     moveDown() {
-//         this.clear()
-//         this.y = this.y + this.sideLength
-//         this.draw()
-//     }
-//
-//     moveRight() {
-//         this.clear()
-//         this.x = this.x + this.sideLength
-//         this.draw()
-//     }
-//
-//     moveLeft() {
-//         this.clear()
-//         this.x = this.x - this.sideLength
-//         this.draw()
-//     }
-// }
