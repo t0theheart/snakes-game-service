f86a893ad007951158e2e621e7c6163a1a917011
t0theheart <35728736+t0theheart@users.noreply.github.com>
2022-05-01 19:12:58 +0300

    test

--- snakes_game_service/app/app.py
+++ snakes_game_service/app/app.py
@@ -49,3 +49,4 @@ async def websocket_endpoint(websocket: WebSocket, user_id: str):
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=5000, log_level="info")
+    # test
